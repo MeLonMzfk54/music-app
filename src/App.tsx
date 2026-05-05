0272b03c6316d4dbc753c7499acd24f51f055298
Garifullin Anton <g42@Mac.cn.ru_novotelecom.ru>
2026-05-05 20:46:07 +0700

    feat: get track info with mockData and fetch

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import {useState, useEffect} from 'react'
 import * as playlistTracksMockData from '../public/mockData/playlist-tracks.json';
+import * as selectedTrackInfoMockData from '../public/mockData/getSelectedTrack.json'
 
 function App() {
     const [selectedTrack, setSelectedTrack] = useState(null);
+    const [selectedTrackInfo, setSelectedTrackInfo] = useState(null);
     const [tracks, setTracks] = useState(null);
 
     useEffect(() => {
+        console.log("useEffect");
         // fetch('https://musicfun.it-incubator.app/api/1.0/playlists/tracks', {
         //     headers: {
         //         'api-key': '37d39d16-0b1a-45be-bc39-dac551910cca',
@@ -19,6 +22,31 @@ function App() {
         }, 1000))
             .then(res => setTracks(res?.data));
     }, [])
+
+    function chooseTrack(track) {
+        console.log('trackId - ', track);
+        if (selectedTrack?.id === track.id) {
+            setSelectedTrack(null);
+            setSelectedTrackInfo(null);
+            return;
+        }
+        setSelectedTrack(track);
+
+        new Promise(resolve => setTimeout(() => {
+            resolve(selectedTrackInfoMockData);
+        }, 1000))
+            .then(res => setSelectedTrackInfo(res));
+
+        // fetch(`https://musicfun.it-incubator.app/api/1.0/playlists/tracks/${track.id}`, {
+        //     headers: {
+        //         'api-key': '37d39d16-0b1a-45be-bc39-dac551910cca',
+        //     }
+        // })
+        //     .then(res => res.json())
+        //     .then(json => setSelectedTrackInfo(json));
+    }
+
+
 
     if (tracks === null) {
         return <div>
@@ -36,20 +64,24 @@ function App() {
     return (
         <>
             <h1>G-Music</h1>
-            <div style={{ display: 'flex' }}>
-                <ul>
+            <div style={{ display: 'flex', gap: '30px'}}>
+                <ul style={{maxWidth: '300px'}}>
                     {tracks.map((track) => (
-                        <li key={track.id} style={{border: track.id === selectedTrack ? '1px solid orange' : 'none'}}>
-                            <div onClick={() => {
-                              return track.id === selectedTrack ? setSelectedTrack(null) : setSelectedTrack(track.id)
-                            }}>{track.attributes.title}</div>
+                        <li key={track.id} style={{border: track.id === selectedTrack?.id ? '1px solid orange' : 'none'}}>
+                            <div style={{overflowX: 'scroll'}} onClick={() => chooseTrack(track)}>{track.attributes.title}</div>
                             <audio src={track.attributes.attachments[0].url} controls></audio>
                         </li>
                     ))}
                 </ul>
-                <div>
-                    <h3>Details</h3>
-                    {selectedTrack === null ? 'Track is not selected' : ''}
+                <div style={{position: 'sticky', top: '10px', maxHeight: '300px'}}>
+                    <h2>Details</h2>
+                    {selectedTrack === null ? 'Track is not selected' : (
+                        <div>
+                            <h3>{selectedTrackInfo?.attributes.title}</h3>
+                            <h4>Lyrics</h4>
+                            <p>{selectedTrackInfo?.attributes.lyrics ? selectedTrackInfo?.attributes.lyrics : 'no lyrics'}</p>
+                        </div>
+                    )}
                 </div>
             </div>
         </>
